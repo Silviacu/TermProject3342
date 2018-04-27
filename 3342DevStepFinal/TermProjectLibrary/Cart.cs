46a75bcc73c483e0a3d13d82b7641b812c341434
xsnowyflake <xsnowyflake@gmail.com>
2018-04-27 16:36:20 -0400

    Vacation Cart + Vacation Item Classes

---
+++ 3342DevStepFinal/TermProjectLibrary/Cart.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TermProjectLibrary
+{
+    [Serializable]
+    public class Cart
+    {
+        private ArrayList hotels;
+        private ArrayList flights;
+        private ArrayList events;
+        private ArrayList cars;
+
+        public Cart()
+        {
+            hotels = new ArrayList();
+            flights = new ArrayList();
+            events = new ArrayList();
+            cars = new ArrayList();
+        }
+
+        public ArrayList Hotels
+        {
+            get { return hotels; }
+            set { hotels = value; }
+        }
+        public ArrayList Flights
+        {
+            get { return flights; }
+            set { flights = value; }
+        }
+        public ArrayList Events
+        {
+            get { return events; }
+            set { events = value; }
+        }
+        public ArrayList Cars
+        {
+            get { return cars; }
+            set { cars = value; }
+        }
+
+        public void AddtoCart(VacationItem item)
+        {
+            if (item.IsHotel)
+                hotels.Add(item);
+            else if (item.IsFlight)
+                flights.Add(item);
+            else if (item.IsEvent)
+                events.Add(item);
+            else
+                cars.Add(item);
+        }
+
+        public void ClearCart()
+        {
+            hotels.Clear();
+            flights.Clear();
+            events.Clear();
+            cars.Clear();
+        }
+
+    }
+}
---
+++ 3342DevStepFinal/TermProjectLibrary/VacationItem.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TermProjectLibrary
+{
+    [Serializable]
+    public class VacationItem
+    {
+        private string description;
+        private double price;
+        private int quantity;
+        private int carID;
+        private int hotelID;
+        private int roomID;
+        private DateTime checkInDate;
+        private DateTime checkOutDate;
+        private int flightID;
+        private int airCarrierID;
+        private int eventID;
+        private bool isCar;
+        private bool isHotel;
+        private bool isFlight;
+        private bool isEvent;
+
+        public VacationItem()
+        {
+            description = "";
+            price = 0;
+            quantity = 1;
+            carID = 0;
+            hotelID = 0;
+            roomID = 0;
+            flightID = 0;
+            eventID = 0;
+        }
+
+        public string CarAgencyName
+        { get; set; }
+
+        public string CarAgencyCity
+        { get; set; }
+
+        public string CarAgencyState
+        { get; set; }
+
+        public int EventAgencyID
+        { get; set; }
+
+        public string Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+
+        public bool IsCar
+        {
+            get { return isCar; }
+            set { isCar = value; }
+        }
+
+        public bool IsHotel
+        {
+            get { return isHotel; }
+            set { isHotel = value; }
+        }
+
+        public bool IsFlight
+        {
+            get { return isFlight; }
+            set { isFlight = value; }
+        }
+
+        public bool IsEvent
+        {
+            get { return isEvent; }
+            set { isEvent = value; }
+        }
+
+        public double Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+
+        public int Quantity
+        {
+            get { return quantity; }
+            set { quantity = value; }
+        }
+
+        public int CarID
+        {
+            get { return carID; }
+            set { carID = value; }
+        }
+
+        public int HotelID
+        {
+            get { return hotelID; }
+            set { hotelID = value; }
+        }
+
+        public int FlightID
+        {
+            get { return flightID; }
+            set { flightID = value; }
+        }
+
+        public int AirCarrierID
+        {
+            get { return airCarrierID; }
+            set { airCarrierID = value; }
+        }
+
+        public int EventID
+        {
+            get { return eventID; }
+            set { eventID = value; }
+        }
+
+        public int RoomID
+        {
+            get { return roomID; }
+            set { roomID = value; }
+        }
+
+        public DateTime CheckInDate
+        {
+            get { return checkInDate; }
+            set { checkInDate = value; }
+        }
+
+        public DateTime CheckOutDate
+        {
+            get { return checkOutDate; }
+            set { checkOutDate = value; }
+        }
+    }
+}
